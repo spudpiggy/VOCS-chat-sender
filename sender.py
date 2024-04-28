ea1f4eb9f0edb38125dd93d53b89e6245d0c2718
spudpiggy <ImQuiteSus@gmail.com>
2024-04-28 19:42:29 +0100

    Added device chooser code

--- sender.py
+++ sender.py
@@ -4,4 +4,26 @@ import argparse
 
 with open('config.toml', 'rb') as f:
 	config = toml.load(f)
-print(config['defaultDevice'])
+	print("Loaded config!")
+
+if config['defaultDevice'] != 0:	# If a default is set
+	device = config['defaultDevice']
+else:
+	tmp_valid = False
+	while not tmp_valid:
+		for n in config['targets']:
+			tmp_dev = config['targets'][str(n)]
+			tmp_name = tmp_dev['name']
+			tmp_ip = tmp_dev['ip']
+			tmp_port = tmp_dev['port']
+			print("""#{}
+			\tName: {}
+			\tIP: {}
+			\tPort: {}""".format(n,tmp_name,tmp_ip,tmp_port))
+		tmp_choice = str(int(input("Choose a device.\n")))
+		try:
+			device = config['targets'][tmp_choice]
+		except:
+			print("Invalid ID! Please try again.")
+		else:
+			tmp_valid = True
